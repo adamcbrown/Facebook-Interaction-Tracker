0cf87669b7803c6b9d91891485bfa87e8101741d
adamcbrown <adamcbrown1997@gmail.com>
2014-08-01 14:46:21 -0400

    Fixing sinatra/mechanize conflict 7

--- app.rb
+++ app.rb
@@ -2,9 +2,9 @@ require 'bundler' #require bundler
 Bundler.require #require everything in bundler in gemfile
 require_relative './lib/facebook_scraper.rb'
 require_relative './lib/mailgun.rb'
-require "mechanize"
 
 get '/' do
+  set :server, 'thin'
   erb :index # This tells your program to use the html associated with the index.erb file in your browser.
 end
 

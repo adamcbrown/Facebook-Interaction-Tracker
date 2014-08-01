bbc980776e9bc9278d1833f66c624066e8f96218
adamcbrown <adamcbrown1997@gmail.com>
2014-08-01 14:29:10 -0400

    Fixing sinatra/mechanize conflict 5

--- app.rb
+++ app.rb
@@ -2,18 +2,20 @@ require 'bundler' #require bundler
 Bundler.require #require everything in bundler in gemfile
 require_relative './lib/facebook_scraper.rb'
 require_relative './lib/mailgun.rb'
+require "sinatra/base"
 
-
-get '/' do
-  set :server, 'webrick'
-  erb :index # This tells your program to use the html associated with the index.erb file in your browser.
-end
-
-post '/' do
-  puts params[:email]
-  if params[:email] and params[:password]
-    @facebook_scraper=FacebookScraper.new(params[:email], params[:password])
-    @facebook_scraper.update_names(5) 
+class MyApp < Sinatra::Base
+  get '/' do
+    set :server, 'webrick'
+    erb :index # This tells your program to use the html associated with the index.erb file in your browser.
   end
-  erb :index
+
+  post '/' do
+    puts params[:email]
+    if params[:email] and params[:password]
+      @facebook_scraper=FacebookScraper.new(params[:email], params[:password])
+      @facebook_scraper.update_names(5) 
+    end
+    erb :index
+  end
 end

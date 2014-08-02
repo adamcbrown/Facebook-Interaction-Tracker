ce08df6799f804a7b2b9bbfaaf266834719065b7
adamcbrown <adamcbrown1997@gmail.com>
2014-08-01 20:15:27 -0400

    Timeout error not fixed, reduced num of names to 10

--- app.rb
+++ app.rb
@@ -9,9 +9,9 @@ get '/' do
 end
 
 post '/' do
-  puts params[:email]
   if params[:email] and params[:password]
     @facebook_scraper=FacebookScraper.new(params[:email], params[:password])
+    @facebook_scraper.update_names(10) 
   end
   erb :index
 end
